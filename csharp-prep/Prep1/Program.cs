bd89569cf7d16bfe378da059696ec509b851ee08
ibukunoluwashina <ibukunoluwashina@gmail.com>
2023-01-17 17:32:41 +0100

    w2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,14 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-       {
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
 
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
+        int guess = -1;
 
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
-    }
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
