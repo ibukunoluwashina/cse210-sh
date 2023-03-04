1a033184249616830f84871424e6e1f414088ba7
ibukunoluwashina <ibukunoluwashina@gmail.com>
2023-03-04 12:23:03 +0100

    week 6

--- prove/Develop03/scriptures.cs
+++ prove/Develop03/scriptures.cs
@@ -2,23 +2,36 @@ using System;
 
 // creat the class
 
-public class newScriptures
+public class Scripture
 {
     // variable declarations so they can be re-used 2 reference
-    public string _scripture;
-    public string _chapter;
-    public string _verse
+    private string _textOfScripture;
+    private Reference _reference;
+    private List<Word> _words;
+    private object puplic;
 
-    public newScriptures(string _)
+    public Scripture(string textOfScripture, Reference reference)
     {
-        
+        _words = new List<Word>();
+        _textOfScripture = textOfScripture;
+        _reference = reference;
+
+        string[] array = _textOfScripture.Split(" ");
+
+        foreach(string text in array)
+        {
+            Word word = new Word(text);
+            _word.Add(word);
+        }
+
+    puplic void HideWord()
+    {
+        foreach(Word word in _words)
+        {
+            word.HideWord();
+        }
+    }
     }
 
-    public void displayQ(string scripture){
-        Console.WriteLine(scripture);
+
     }
-
-
-
-
-}
