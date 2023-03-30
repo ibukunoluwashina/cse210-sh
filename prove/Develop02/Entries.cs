4f0cb4b7fde83a1d33b2fe208706b26b29f30d21
ibukunoluwashina <ibukunoluwashina@gmail.com>
2023-03-30 15:46:08 +0100

    journal

--- prove/Develop02/Entries.cs
+++ prove/Develop02/Entries.cs
@@ -1,16 +1,7 @@
 using System;
 
 public class Entry{
-    public DateTime date;
-    public string content;
-
-    public Entry()
-    {
-    }
-
-    public Entry(DateTime date, string content)
-    {
-        this.date = date;
-        this.content = content;
-    }
+    public string Prompt {get; set;}
+    public string Response { get; set;}
+    public DateTime Date { get; set;}
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,53 +1,92 @@
 public class Program
 {
-
+    static List<Entry> journal = new List<Entry>();
+    static List<string> prompts = new List<string>(){
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
     static void Main(string[] args){
-        Journal journal = new Journal();
+               while(true){
+                Console.WriteLine("Menu Option:");
+                Console.WriteLine("1. Write");
+                Console.WriteLine("2. Display");
+                Console.WriteLine("3. Save");
+                Console.WriteLine("4. Load");
+                Console.WriteLine("5. Exit");
 
-        while (true) {
-            Console.WriteLine("\n Please select one of the following choice: ");
-            Console.WriteLine("1. Write");
-            Console.WriteLine("2. Display");
-            Console.WriteLine("3. Save");
-            Console.WriteLine("4. Load");
-            Console.WriteLine("5. Quit");
+                string choice = Console.ReadLine();
 
-            string choiceStr = Console.ReadLine();
-            int choice = int.Parse(choiceStr);
+                switch(choice) {
+                    case "1":
+                        WriteNewEntry();
+                        break;
 
-            switch (choice) {
-                case 1:
-                    DateTime date = PrompGenerator.GetDate();
-                    string content = PrompGenerator.GetContent();
-                    journal.AddEntry(date, content);
-                    break;
+                    case "2":
+                        DisplayJournal();
+                        break;
 
-                case 2:
-                    journal.DisplayEntries();
-                    break;
+                    case "3":
+                        SaveJournalToFile();
+                        break;
 
-                case 3:
-                    Console.WriteLine("Enter a file name: ");
-                    string fileNameSave = Console.ReadLine();
-                    journal.saveToFile(fileNameSave);
-                    break;
+                    case "4":
+                        LoadJournalFromFile();
+                        break;
 
-                case 4:
-                Console.WriteLine("Enter a filename: ");
-                string fileNameLoad = Console.ReadLine();
-                string dateStr = Console.ReadLine();
-                ReadOnlySpan<char> dateStrSpan = new ReadOnlySpan<char>(dateStr.ToCharArray());
-                journal.LoadFromFile(fileNameLoad, dateStrSpan);
-                break;
+                    case "5":
+                        return;
 
-                case 5:
-                    return;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+               }
+         }
+         static void WriteNewEntry(){
+            Random random = new Random();
+            string prompt = prompts[random.Next(prompts.Count)];
+            Console.WriteLine(prompt);
+            string response = Console.ReadLine();
+            Entry entry = new Entry(){
+                Prompt = prompt,
+                Response = response,
+                Date = DateTime.Now
+            };
+            journal.Add(entry);
+         }
 
-                default:
-                    Console.WriteLine("invalid choice.");
-                    break;
-
+         static void DisplayJournal(){
+            foreach (Entry entry in journal) {
+                Console.WriteLine("{0} ({1}): {2}", entry.Prompt, entry.Date, entry.Response);
             }
-        }
-    }
+         }
+         static void SaveJournalToFile(){
+            Console.Write("Enter a file name: ");
+            string fileName = Console.ReadLine();
+            using (StreamWriter writer = new StreamWriter(fileName)){
+                foreach (Entry entry in journal){
+                    writer.WriteLine("{0}\t{1}\t{2}", entry.Prompt, entry.Date, entry.Response);
+                }
+            }
+         }
+         static void LoadJournalFromFile(){
+            Console.Write("Enter a file name: ");
+            string fileName = Console.ReadLine();
+            List<Entry> newJournal = new List<Entry>();
+            using (StreamReader reader = new StreamReader(fileName)){
+                string line;
+                while((line = reader.ReadLine()) != null){
+                    string[] parts = line.Split('\t');
+                    var entry = new Entry(){
+                        Prompt = parts[0],
+                        Date = DateTime.Parse(parts[1]),
+                        Response = parts[2]
+                    };
+                    newJournal.Add(entry);
+                }
+            }
+         }
 }
